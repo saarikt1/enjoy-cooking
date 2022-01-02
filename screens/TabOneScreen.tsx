6b1dd841d8e8258d7bbefea4b0562a9f5f5c204d
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2022-01-02 18:54:35 +0200

    Add a hardcoded recipe view

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,30 @@
 import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={styles.title}>Kookoscurry</Text>
+      <Text style={styles.subtitle}>Ingredients</Text>
+      <Text>
+        {`
+        1 dl vihreitä linssejä
+        1 sipuli
+        2 valkosipulinkynttä
+        tuoretta chiliä maun mukaan
+        2 tl kookosöljyä
+        0,5 tl garam masalaa
+        2 tl currytahnaa
+        3 kaffirlimen lehteä
+        2 dl tomaattimurskaa
+        1 tlk kikherneitä
+        2 dl kookosmaitoa
+        0,5 limen mehu
+        tummaa riisiä`}
+      </Text>
     </View>
   );
 }
@@ -17,16 +32,14 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
+    margin: 24,
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  subtitle: {
+    marginLeft: 24,
+    fontSize: 16,
   },
 });
