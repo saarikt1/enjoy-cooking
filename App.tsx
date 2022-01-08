46b6b5625118e079641c5e7ee774e2af5a75d7ed
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2022-01-08 10:46:11 +0200

    Simplify the boilerplate code

--- App.tsx
+++ App.tsx
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  }
   return (
-    <SafeAreaProvider>
-      <Navigation colorScheme={colorScheme} />
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>Kookoscurry</Text>
+        <Text style={styles.subtitle}>Ingredients</Text>
+        <Text>
+          {`
+        1 dl vihreitä linssejä
+        1 sipuli
+        2 valkosipulinkynttä
+        tuoretta chiliä maun mukaan
+        2 tl kookosöljyä
+        0,5 tl garam masalaa
+        2 tl currytahnaa
+        3 kaffirlimen lehteä
+        2 dl tomaattimurskaa
+        1 tlk kikherneitä
+        2 dl kookosmaitoa
+        0,5 limen mehu
+        tummaa riisiä`}
+        </Text>
+      </View>
       <StatusBar />
-    </SafeAreaProvider>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  title: {
+    margin: 24,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    marginLeft: 24,
+    fontSize: 16,
+  },
+});
--- components/StyledText.tsx
+++
@@ -1,8 +0,0 @@
-import { Text, TextProps } from './Themed';
-
-export default function MonoText(props: TextProps) {
-  return (
-    // eslint-disable-next-line react/destructuring-assignment,react/jsx-props-no-spreading
-    <Text {...props} style={[props.style, { fontFamily: 'space-mono' }]} />
-  );
-}
