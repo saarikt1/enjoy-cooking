674bdefd3db563a81e1efa0f14b420a42dcf7f79
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2022-01-08 10:54:43 +0200

    Move RecipeView to its own component

--- App.tsx
+++ App.tsx
@@ -1,29 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import RecipeView from './RecipeView';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text style={styles.title}>Kookoscurry</Text>
-        <Text style={styles.subtitle}>Ingredients</Text>
-        <Text>
-          {`
-        1 dl vihreitä linssejä
-        1 sipuli
-        2 valkosipulinkynttä
-        tuoretta chiliä maun mukaan
-        2 tl kookosöljyä
-        0,5 tl garam masalaa
-        2 tl currytahnaa
-        3 kaffirlimen lehteä
-        2 dl tomaattimurskaa
-        1 tlk kikherneitä
-        2 dl kookosmaitoa
-        0,5 limen mehu
-        tummaa riisiä`}
-        </Text>
-      </View>
+      <RecipeView />
       <StatusBar />
     </SafeAreaView>
   );
@@ -32,14 +14,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  title: {
-    margin: 24,
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  subtitle: {
-    marginLeft: 24,
-    fontSize: 16,
   },
 });
---
+++ RecipeView.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function RecipeView() {
+  return (
+    <View>
+      <Text style={styles.title}>Kookoscurry</Text>
+      <Text style={styles.subtitle}>Ingredients</Text>
+      <Text>
+        {`
+        1 dl vihreitä linssejä
+        1 sipuli
+        2 valkosipulinkynttä
+        tuoretta chiliä maun mukaan
+        2 tl kookosöljyä
+        0,5 tl garam masalaa
+        2 tl currytahnaa
+        3 kaffirlimen lehteä
+        2 dl tomaattimurskaa
+        1 tlk kikherneitä
+        2 dl kookosmaitoa
+        0,5 limen mehu
+        tummaa riisiä`}
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    margin: 24,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    marginLeft: 24,
+    fontSize: 16,
+  },
+});
